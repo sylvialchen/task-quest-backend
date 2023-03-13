97aec51fdcdfa31664cb0b18bd22657cd67f3f03
Sylvia Chen <sylvialchen33@gmail.com>
2023-03-13 16:12:19 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+// DEPENDENCIES
+require("dotenv").config();
+const { PORT = 4000, MONGODB_URL } = process.env;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const methodOverride = require('method-override');
+
+
+// DATABASE CONNECTION
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+// Connection Events
+mongoose.connection
+    .on("open", () => console.log("You are connected to mongoose"))
+    .on("close", () => console.log("You are disconnected from mongoose"))
+    .on("error", (error) => console.log(error));
+
+// MiddleWare
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+app.use(methodOverride('_method'));
+
+// Routes / Controllers
+
+
+
+
+
+// LISTENER
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
