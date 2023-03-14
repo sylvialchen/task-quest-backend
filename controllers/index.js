4ca2a65240720acd2eb7eb56804c1b131b46245f
EmilyJarecki <jareckiemily@yahoo.com>
2023-03-14 11:30:18 -0500

    tried to fix errors

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
 module.exports = {
-
-    Reward: require('./Reward'),
+    Reward: require('./rewardCtrl'),
     caregiverCtrl: require("./caregiverCtrl"),
     childCtrl: require('./childCtrl')
-
 };
 
 mongoose.connect(process.env.MONGODB_URL);
--- controllers/rewardCtrl.js
+++ controllers/rewardCtrl.js
@@ -25,8 +25,38 @@ router.post("/", verifyToken, async (req, res, next) =>  {
 });
 
 
+///yet to test
+router.get("/:id", async (req, res, next) => {
+	try {	
+		const singleReward = await Reward.findById(req.params.id)
+		console.log(singleReward, "the single reward")
+		res.status(200).json(singleReward)
+	}catch(error){
+		res.status(400).json({error: "error"})
+        return next(err)
+	}
+});
 
-
+router.put("/:id", async (req, res, next) => {
+	try{
+		const updatedTweet = await Tweet.findByIdAndUpdate(req.params.id, req.body)
+		console.log(updatedTweet)
+		res.status(200).json({message: "Successfully updated tweet", updatedTweet})
+	}catch(error){
+		res.status(400).json({error: "error"})
+        return next(err)
+	}
+});
+router.delete("/:id", async (req, res, next) => {
+	try{
+		const deletedTweet = await Tweet.findByIdAndDelete(req.params.id)
+		console.log(deletedTweet)
+		res.status(200).json({message: "Deleted Tweet", deletedTweet })	
+	}catch(err){
+		res.status(400).json({error: "error"})
+        return next(err)
+	}
+});
 
 
 module.exports = router
