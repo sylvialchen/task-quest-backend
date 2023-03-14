1e9041749fa323128c103cdcfc33e75d66facaf2
daryacoding <abdullahzadeh.darya@gmail.com>
2023-03-13 21:29:11 -0400

    added route for all tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {dataController, apiController} = require('../controllers/taskCtrl');
 
+// Index
+router.get('/', dataController.index, apiController.index)
 // Index not completed
-router.get('/', dataController.indexNotComplete, apiController.index);
+router.get('/incompleted', dataController.indexNotComplete, apiController.index);
 // Index completed
 router.get('/completed', dataController.indexComplete, apiController.index);
 // Delete
@@ -14,5 +16,8 @@ router.put('/:id', dataController.update, apiController.show);
 router.post('/', dataController.create, apiController.show);
 // Show 
 router.get(':/id', dataController.show, apiController.show);
-
+/* // Add Task for Child
+router.post()
+// Remove Task for Child
+router.delete('/') */
 module.exports = router;
